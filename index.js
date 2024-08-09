9c33d41070e86187d877ee2f9d0a5097411fb908
Aditya <adityag860libra@gmail.com>
2024-08-09 21:41:52 +0530

    mvc model

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+const userRoute = require("./routes/user")
+app.use("/user", userRoute)
+
 app.listen(3000, () => {
     console.log("Server running on port 3000")
 })
